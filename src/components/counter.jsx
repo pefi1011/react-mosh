01bf326298ec5d653dae14d7b7e692447250c345
Filip S. Perisic <filip.s.perisic@sap.com>
2019-10-26 20:08:03 +0200

    Setting state values of a component through the props object

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 1,
+    count: this.props.value,
     tags: ["tag1", "tag2", "tag3"]
   };
 
@@ -12,35 +12,30 @@ class Counter extends Component {
   };
 
   render() {
+    // Every React component has a property "props"
+    // which is a plain JS object that includes all attributes
+    // set in the "consumer" component, i.e. in this case the "Counters" component
+
+    // "key" attribute is not part of "props" objects because its a special attribute
+    console.log("props", this.props);
+
     return (
-      <React.Fragment>
+      <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          // Instead of writing a wrapper function (doHandleIncrement)
-          // we can use an inline function
-          // i.e. we pass the arrow function (the previous wrapper function (doHandleIncrement) )
-          // We are still passing the function reference. The reference is though to the arrow function which we use as the wrapper
-
-          // When rendering a list of products in a shopping cart,
-          // in our map method, we have access to a product object
-          // so you can the product which is being currently rendered as tha argument
           onClick={() => this.handleIncrement()}
-          // IN A NUTSHELL
-          // whenever you need to pass an argument to your event handler,
-          // simply wrapper the event handler with an arrow function
-          // and in the body of the arrow function call the event handler and pass the argument
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
-      </React.Fragment>
+      </div>
     );
   }
 
   formatCount() {
     // Object destructuring
     const { count } = this.state;
-    const emptyCount = <h1>Zero</h1>;
+    const emptyCount = <p>Zero</p>;
 
     return count === 0 ? emptyCount : count;
   }
