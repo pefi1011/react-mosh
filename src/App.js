a2ed60b4e53ec03f4978bcd99e9a61190b1ac54f
Filip S. Perisic <filip.s.perisic@sap.com>
2019-11-01 21:14:11 +0100

    Lifted the state up: from the Counters to the App

--- src/App.js
+++ src/App.js
@@ -1,17 +1,70 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import NavBar from "./components/navbar";
 import Counters from "./components/counters";
 
-function App() {
-  return (
-    <React.Fragment>
-      <NavBar />
-      <main className="container">
-        <Counters></Counters>
-      </main>
-    </React.Fragment>
-  );
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
+  };
+
+  handleIncrement = counter => {
+    console.log("counter: ", counter);
+
+    // using the spread operator to clone the array
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter); // getting the index by object itself
+    counters[index] = { ...counter }; // using the spread operator to clone the argument, i.e. the counter object
+    counters[index].value++;
+
+    console.log("counter incremented: ", this.state.counters[index]);
+
+    this.setState({ counters });
+  };
+
+  handleDelete = counterId => {
+    // get a new array without the counter with the given id
+    const counters = this.state.counters.filter(
+      counter => counter.id !== counterId
+    );
+
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map(counter => {
+      counter.value = 0;
+      return counter;
+    });
+    console.log("counters: ", counters);
+
+    this.setState({ counters });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Counters
+            // these are the 3 events raised by this component and their handlers
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+            counters={this.state.counters}
+            // The counter component is now a controlled component
+            // because it does not have any state
+            // it simply receives the data and methods to modify the data via props
+          />
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,81 +1,24 @@
 import React, { Component } from "react";
 import Counter from "./counter";
 
-// PROPS: data which is given to the component -> INPUT TO THE COMPONENT
-//    -> Props is read-only: we cannot change the input of the component within a component
-// STATE: data which is local/private to a component
-//    -> Other components cannot access that state! It's completely internal to that component
-//
-//
-//
-//
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 4 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 }
-    ]
-  };
-
-  handleIncrement = counter => {
-    console.log("counter: ", counter);
-    // Like before we do not update the state directly
-    // we crate a new counters array and give it to the setState method
-    // and let React deal with updating the state
-
-    // using the spread operator to clone the array
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter); // getting the index by object itself
-    counters[index] = { ...counter };
-    counters[index].value++;
-
-    console.log("counter incremented: ", this.state.counters[index]);
-
-    this.setState({ counters });
-  };
-
-  handleDelete = counterId => {
-    // get a new array without the counter with the given id
-    const counters = this.state.counters.filter(
-      counter => counter.id !== counterId
-    );
-
-    this.setState({ counters });
-  };
-
-  handleReset = () => {
-    console.log("Handle reset");
-
-    // make a new array out of the counters array
-    // set the value of counter to 0 and add it to the "mapped" array
-    const counters = this.state.counters.map(counter => {
-      counter.value = 0;
-      return counter;
-    });
-
-    this.setState({ counters });
-
-    // NOTHING HAPPENS BECAUSE WE DO NO HAVE THE SINGLE SOURCE OF TRUTH!!!
-    // Each of our components (4 instances of Counter) have their own local state
-    // Our counters component (array of counter) and each counter component has a value
-    // This value is currently disconnected from the value property of each counter object which we have in the array
-  };
-
   render() {
     return (
       <div>
         <button
-          onClick={this.handleReset}
+          onClick={this.props.onReset}
           className="btn btn-primary btn-sm m-2"
         >
           Reset
         </button>
-        {this.state.counters.map(counter => (
+        {this.props.counters.map(counter => (
           <Counter
-            onDelete={this.handleDelete} // PASSING THE REFERENCE (TO THE METHOD) TO THE CHILD COMPONENT VIA PROPS
-            onIncrement={this.handleIncrement}
+            // The counter component is rasing the onDelete event
+            // and we are bubbling that event to the parent
+            // so we are not handling this event in the component, we are bubbling it up
+            // to the parent of this component
+            onDelete={this.props.onDelete}
+            onIncrement={this.props.onIncrement}
             key={counter.id}
             counter={counter}
             selected={true}
