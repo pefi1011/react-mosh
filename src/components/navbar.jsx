aa278ccc1a0ef13f2550b4cc430370aaab23d5ef
Filip S. Perisic <filip.s.perisic@sap.com>
2019-11-01 21:23:31 +0100

    Functional Components Idea

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
 
 class NavBar extends Component {
+  // Our NavBar component has only the render method
+  // we don't have any event handlers
+  // we don't have any helper methods to calculate a value
+  // we don't have any state -> we get all the data via props
+  // the only thing we have is the single render method
+  // ===> in such case we can convert this component into STATELESS FUNCTIONAL COMPONENT
+  // i.e. instead of using a Class to define this component, we can use a function!
+
   render() {
     return (
       <nav className="navbar navbar-light bg-light">
