7731f356f5e761202392f2fc41ee12a50a315472
Filip S. Perisic <filip.s.perisic@sap.com>
2019-11-01 21:36:11 +0100

    Applying object Destructuring to destruct the props argument

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,44 +1,19 @@
 import React, { Component } from "react";
 
-// Add props as parameter
-const NavBar = props => {
+// Using Object Destructuring method
+// to destruct the props argument
+// so that we do not have to write props.totalCounters within the SFC
+const NavBar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="#">
         Navbar{" "}
         <span className="badge badge-pill badge-secondary">
-          {/* this.props.totalCounters}{" "}
-          {/** 1. This works only in class component. In functional component we have to define props as param */}
-          {props.totalCounters}{" "}
-          {/** remove "this" because props is now the argument of the function */}
+          {totalCounters}
         </span>
       </a>
     </nav>
   );
 };
-
-/*
-class NavBar extends Component {
-  // Our NavBar component has only the render method
-  // we don't have any event handlers
-  // we don't have any helper methods to calculate a value
-  // we don't have any state -> we get all the data via props
-  // the only thing we have is the single render method
-  // ===> in such case we can convert this component into STATELESS FUNCTIONAL COMPONENT
-  // i.e. instead of using a Class to define this component, we can use a function!
-
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar{" "}
-          <span className="badge badge-pill badge-secondary">
-            {this.props.totalCounters}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}*/
 
 export default NavBar;
